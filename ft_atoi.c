406491af59c68d0724a7a370cad2f0f45e1d20ff
jo0jmjm <130815812+jo0jmjm@users.noreply.github.com>
2023-06-04 20:23:24 +0900

    ft_atoi.c を更新

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,41 +12,4 @@
 
 #include "libft.h"
 
-static int	is_space(char c)
-{
-	return (c == ' ' || (c >= '\t' && c <= '\r'));
-}
 
-static int	parse_number(const char *str, int *index)
-{
-	long long	result;
-	int			sign;
-
-	result = 0;
-	sign = 1;
-	while (is_space(str[*index]))
-		(*index)++;
-	if (str[*index] == '-')
-	{
-		sign = -1;
-		(*index)++;
-	}
-	else if (str[*index] == '+')
-		(*index)++;
-	while (ft_isdigit(str[*index]))
-	{
-		result = result * 10 + (str[*index] - '0');
-		(*index)++;
-	}
-	return (result * sign);
-}
-
-int	ft_atoi(const char *str)
-{
-	int	index;
-
-	if (str == NULL)
-		return (INT_MIN);
-	index = 0;
-	return (parse_number(str, &index));
-}
