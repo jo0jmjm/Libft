4c8e2381c0ea2d94b9d647ce32efbbef448a2e3a
jo0jmjm <130815812+jo0jmjm@users.noreply.github.com>
2023-06-04 20:23:52 +0900

    ft_atoi.c を更新

--- ft_atoi.c
+++ ft_atoi.c
@@ -13,3 +13,31 @@
 #include "libft.h"
 
 
+int			ft_atoi(const char *str)
+{
+	size_t				i;
+	int					sign;
+	unsigned long int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while ((str[i] != '\0') && ft_isdigit(str[i]))
+	{
+		if ((result > 922337203685477580 || (result == 922337203685477580
+			&& (str[i] - '0') > 7)) && sign == 1)
+			return (-1);
+		else if ((result > 922337203685477580 || (result == 922337203685477580
+			&& (str[i] - '0') > 8)) && sign == -1)
+			return (0);
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return ((int)(result * sign));
+}
